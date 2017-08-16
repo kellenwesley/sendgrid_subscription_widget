9a2820ac17044304dd5c2cc71e318decd2b054f4
kellenwesley <31050638+kellenwesley@users.noreply.github.com>
2017-08-16 09:13:20 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ exports.senderName = "Arizona State Board of Education";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = all_contacts;
+exports.listId = null;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
