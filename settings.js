eeeec35a3e07b8dabde5682e3f282c0a426c3fd3
kellenwesley <31050638+kellenwesley@users.noreply.github.com>
2017-08-16 08:37:56 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 // Change the url to the domain of your app
-exports.url = 'https://newslettersubscription.herokuapp.com/';
+exports.url = 'https://newslettersubscription.herokuapp.com';
 
 exports.senderEmail = "inbox@azsbe.az.gov";
 exports.senderName = "Arizona State Board of Education";
@@ -10,7 +10,7 @@ exports.listId = all_contacts;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
-exports.templateId = null;
+exports.templateId = dbc810ec-b776-4345-b0c7-02e2bbcd2ab4;
 
 // receive an email when a new signup is confirmed
 exports.sendNotification = true;
