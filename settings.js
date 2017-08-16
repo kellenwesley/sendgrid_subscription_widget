f56ee77618df0b4c737fb08ff3d5309f4884c4b9
kellenwesley <31050638+kellenwesley@users.noreply.github.com>
2017-08-16 10:04:43 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ exports.listId = null;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
-exports.templateId = "0KoGXJk5R9WqE_HtsHnHhQ"
+exports.templateId = "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"
 
 // receive an email when a new signup is confirmed
 exports.sendNotification = true;
